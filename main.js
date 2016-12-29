884500e3e1076dfb241153bd04c2e13000dbd3fb
Ihor Motornyi <igormotorny1997@gmail.com>
2016-12-29 13:52:30 +0200

    Initial version

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow () {
+  win = new BrowserWindow({ width: 350, height: 300, frame: false, transparent: true });
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.on('closed', () => win = null)
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+})
