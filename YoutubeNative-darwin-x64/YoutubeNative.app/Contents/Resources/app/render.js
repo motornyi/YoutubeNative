f08c389f58c7609c931d88c0c2926dd5902fca45
Ihor Motornyi <igormotorny1997@gmail.com>
2017-01-11 17:12:17 +0200

    Fixed errors

---
+++ YoutubeNative-darwin-x64/YoutubeNative.app/Contents/Resources/app/render.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', attachCallback);
+
+function attachCallback() {
+  document.querySelector('#video-input')
+    .addEventListener('keydown', (e) => {
+      if (e.keyCode != 13) return;
+
+      setUrl(e.target.value);
+      // e.target.value = '';
+    });
+}
+
+function setUrl(url) {
+  document.querySelector('iframe')
+    .src = `https://www.youtube.com/embed/${ parseUrl(url) }`
+}
+
+function parseUrl(url) {
+  return url.match(/\?v=[^&]+/)[0].replace(/(\?v=)|&/g, '');
+}
